dd14e02040ac332272469275f9d437be93b2cb6d
GitHub Action <action@github.com>
2021-08-04 01:33:15 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/leereichardt/stenciljscomponents/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.leereichardt.stenciljscomponents.services
+
+import com.github.leereichardt.stenciljscomponents.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/leereichardt/stenciljscomponents/services/MyProjectService.kt
+++ src/main/kotlin/com/github/leereichardt/stenciljscomponents/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.leereichardt.stenciljscomponents.services
 
+import com.github.leereichardt.stenciljscomponents.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
